9caf424f68b3c1f10bbf7cf85f44e8116f893385
Carlos Araújo <car_soad@hotmail.com>
2025-05-10 23:32:26 -0300

    remove node_modules

--- cypress/e2e/3-alurapic/api-alurapic.cy.js
+++ cypress/e2e/3-alurapic/api-alurapic.cy.js
@@ -2,7 +2,7 @@ describe('Buscar fotos e dados', ()=> {
     it('buscar fotos do Flavio', ()=> {
         cy.request({
             method: 'GET', 
-            url: 'https://apialurapic.herokuapp.com/flavio/photos'
+            url: 'https://www.google.comapialurapic.herokuapp.com/flavio/photos'
         }).then((res) =>{
             expect(res.status).to.be.equal(200)
             expect(res.body).is.not.empty
